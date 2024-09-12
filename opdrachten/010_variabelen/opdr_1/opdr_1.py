d1badf313cc032b75b98df9d974c65a5c5584fcc
Fjer29 <97093080@st.deltion.nl>
2024-09-12 15:40:51 +0200

    Eerste draft

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student: Stefan
+# Groep: 4ITX7
 
 # Hier komt je code...
 
-my_list = sdfsidhfdbn
+my_list = list[13, "wat een weertje", 12.5, 8]
+print(my_list)
