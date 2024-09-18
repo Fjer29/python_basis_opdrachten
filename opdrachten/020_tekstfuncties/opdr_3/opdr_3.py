ee244445ba56da236a11b28037f7b27586d2ca23
Fjer29 <97093080@st.deltion.nl>
2024-09-18 16:29:06 +0200

    020-opdr_3-V2

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -7,21 +7,21 @@
 #for i in range(5):
 #    print("    *")
 #    print("   ***")
-#    print("  ******")
-#    print(" ********")
-#    print("***********")
-#    print("    ***")
-#    print("    ***")
-#    print("    ***")
+#    print("  *****")
+#    print(" *******")
+#    print("*********")
+#    print("   ***")
+#    print("   ***")
+#    print("   ***")
 
-result1="    *      " * 5
-result2="   ***     " * 5
-result3="  ******   " * 5
-result4=" ********  " * 5
-result5="***********" * 5
-result6="    ***    " * 5
-result7="    ***    " * 5
-result8="    ***    " * 5
+result1="    *    " * 5
+result2="   ***   " * 5
+result3="  *****  " * 5
+result4=" ******* " * 5
+result5="*********" * 5
+result6="   ***   " * 5
+result7="   ***   " * 5
+result8="   ***   " * 5
 
 print(result1)
 print(result2)
@@ -31,3 +31,9 @@ print(result5)
 print(result6)
 print(result7)
 print(result8)
+
+#result = ""
+#for _ in range(5):
+#    result += "hello world"
+#
+#print(result)
