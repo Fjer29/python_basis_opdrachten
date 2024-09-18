d84b411fa0ef76a41fae755f9b82ba0b25366928
Fjer29 <97093080@st.deltion.nl>
2024-09-18 16:13:27 +0200

    020-opdr_3

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,31 @@
 # Groep:
 
 # Hier komt je code...
+
+#for i in range(5):
+#    print("    *")
+#    print("   ***")
+#    print("  ******")
+#    print(" ********")
+#    print("***********")
+#    print("    ***")
+#    print("    ***")
+#    print("    ***")
+
+result1="    *      " * 5
+result2="   ***     " * 5
+result3="  ******   " * 5
+result4=" ********  " * 5
+result5="***********" * 5
+result6="    ***    " * 5
+result7="    ***    " * 5
+result8="    ***    " * 5
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
