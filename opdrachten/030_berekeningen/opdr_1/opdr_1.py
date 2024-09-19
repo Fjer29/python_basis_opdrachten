dbf72c768a0d3db418e3904aeb248c3161c45296
Fjer29 <97093080@st.deltion.nl>
2024-09-19 14:43:03 +0200

    030-Opdr_1

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,14 @@
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+totaal = [joe, bob, fred]
+totale_leeftijd = sum(totaal)
+print("De totale waarde van de leeftijden is", totale_leeftijd)
+
+gem_leeftijd = int(sum(totaal)/len(totaal))
+print("de gemiddelde leeftijd is", gem_leeftijd)
