3e45730af968254fca328d3cbaedac3571e383e5
Fjer29 <97093080@st.deltion.nl>
2024-09-24 14:49:03 +0200

    030-opdr_2

--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -4,7 +4,11 @@
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = 62.2
+f = 96
 
-print()
+ctof = str(round(c * 1.8 + 32, 1))
+ftoc = str(round((f - 32)/1.8, 1))
+
+print(c, "graden celsius is gelijk aan", ctof, "graden fahrenheit")
+print(f, "graden fahrenheit is gelijk aan", ftoc, "graden fahrenheit")
