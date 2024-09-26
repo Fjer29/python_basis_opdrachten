cf8b3c45c207b1559358bf3bd8f1cdf7f6ea9d03
Fjer29 <97093080@st.deltion.nl>
2024-09-26 15:16:27 +0200

    050-opdr_3

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,16 @@
 
 # Hier komt je code...
 
+steden = []
+for _ in range(5):
+    stad = input("Voer een stad in: ")
+    steden.append(stad)
+
+#list.append(input("Stad 1: "))
+#list.append(input("Stad 2: "))
+#list.append(input("Stad 3: "))
+#list.append(input("Stad 4: "))
+#list.append(input("Stad 5: "))
+
+steden.sort(reverse=True)
+print(steden)
