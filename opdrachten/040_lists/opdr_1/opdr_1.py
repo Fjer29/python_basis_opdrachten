da1c290291e59b75f7b445a52b7d9d25cd550982
Fjer29 <97093080@st.deltion.nl>
2024-09-26 14:03:16 +0200

    040-opdr_1

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,28 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+
+dict_1 = {
+    "id": 0,
+    "voornaam": "Donald",
+    "achternaam": "Duck"
+}
+dict_2 = {
+    "id": 1,
+    "voornaam": "Guus",
+    "achternaam": "Geluk"
+}
+dict_3 = {
+    "id": 2,
+    "voornaam": "Mickey",
+    "achternaam": "Mouse"
+}
+dict_4 = {
+    "id": 3,
+    "voornaam": "Karel",
+    "achternaam": "Paardepoot"
+}
+
+mylist = list[dict_1, dict_2, dict_3, dict_4]
+
+print(mylist)
