e051bfba50046d49bdc61895c7161171f6483e97
Fjer29 <97093080@st.deltion.nl>
2024-10-09 09:36:09 +0200

    060-opdr_4

--- opdrachten/060_loops/opdr_4/opdr_4.py
+++ opdrachten/060_loops/opdr_4/opdr_4.py
@@ -6,4 +6,14 @@
 
 # Hier start de for-loop
 
-my_list = []
+#my_list = []
+
+#for x in range(1, 10):
+#    y = 4 * x + 7
+#    my_list.append(y)
+
+#print(my_list)
+
+
+my_list = [4 * x + 7 for x in range(1, 10)]
+print(my_list)
