3732205db1b64844ffaa981c4e6328991904cdd0
Fjer29 <97093080@st.deltion.nl>
2024-10-10 14:10:40 +0200

    070-opdr_4

--- opdrachten/070_condities/opdr_4/opdr_4.py
+++ opdrachten/070_condities/opdr_4/opdr_4.py
@@ -5,8 +5,19 @@
 
 
 toppings = [("olijven", 4.50), ("kaas", 3.50), ("salami", 3.00), ("pepperoni", 2.00) , ("ansjovis", 2.50)]
-beschikbare_toppings = ...
+beschikbare_toppings = [topping[0] for topping in toppings]
 
 keuze = input(f"Maak een keuze uit onze toppings: {beschikbare_toppings} \n")
 
 # Hier de rest van jouw code...
+kosten = None
+for topping in toppings:
+    if topping[0] == keuze:
+        kosten = topping[1]
+        break
+
+
+if kosten is not None:
+    print("u heeft", keuze, "besteld, dat kost", kosten)
+else:
+    print("Uw keuze zit niet in ons assortiment")
