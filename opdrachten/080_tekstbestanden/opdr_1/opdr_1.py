7a4960ad45af5bd0c845b2dc05bffdc6f8a65de0
Fjer29 <97093080@st.deltion.nl>
2024-10-10 14:25:59 +0200

    080-opdr_1

--- opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
+++ opdrachten/080_tekstbestanden/opdr_1/opdr_1.py
@@ -3,3 +3,12 @@
 # Groep:
 
 # Jouw code komt hier
+
+vraag1 = input("Wat vind je van de huidige regering? ")
+vraag2 = input("Wat vind je van de Python lessen tot nu toe? ")
+vraag3 = input("Wat vind jij de mooiste stad van Nederland? ")
+
+file1 = open("vragenlijst.txt", "at")
+file1.write(f"{vraag1} \n")
+file1.write(f"{vraag2} \n")
+file1.write(f"{vraag3} \n")
