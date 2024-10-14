8f84f3bc9034995725e58749de4c74746c8ac301
Fjer29 <97093080@st.deltion.nl>
2024-10-14 16:13:04 +0200

    080-opdr_3

--- opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
+++ opdrachten/080_tekstbestanden/opdr_3/opdr_3.py
@@ -4,3 +4,42 @@
 
 
 
+#print(ord(' '))
+#print(chr(122))
+
+zin = input("Geef de tekst die je wilt encrypten: ")
+
+character_codes = []
+for teken in zin:
+    if teken == ' ':
+        character_codes.append(teken)
+    elif 'a' <= teken <= 'z':
+        new_code = ord(teken) + 5
+        if new_code > ord('z'):
+            new_code -= 26
+        character_codes.append(chr(new_code))
+    elif 'A' <= teken <= 'Z':
+        new_code = ord(teken) + 5
+        if new_code > ord('Z'):
+            new_code -= 26
+        character_codes.append(chr(new_code))
+    else:
+        character_codes.append(teken)
+
+encrypted_zin = ''.join(character_codes)
+
+print(encrypted_zin)
+
+
+
+
+#character_codes = [ord(teken) for teken in zin]
+#encrypted_tekst = []
+
+#for character in character_codes:
+#    character = character + 5
+#    encrypted_tekst.append(character)
+
+#encrypted_zin = ''.join(chr(code) for code in encrypted_tekst)
+
+#print(encrypted_zin)
